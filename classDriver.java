1d26e5088ee658f8c81b47c48193a982daac453a
HaniHan <157235849+hanlee53@users.noreply.github.com>
2024-02-26 21:40:51 -0500

    Add files via upload

---
+++ classDriver.java
@@ -0,0 +1,19 @@
+ 
+/**
+ *@author SeyedParsa Hejazi
+ *@author Han Lee
+ */
+import java.io.IOException;
+
+public class classDriver {
+	/**
+	 *
+	 * This is the Main class that will integrate every other class to functionate the programs.
+	 * */
+	public static void main(String[] args) throws Exception{
+		classPart1.do_part1();
+		classPart2.do_part2();
+		classPart3.do_part3();
+	}
+	
+}
